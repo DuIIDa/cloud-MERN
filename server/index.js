d8416b236664ff5a600207362997421385e2df9a
Dudarev <dima_dudariev@mail.ru>
2020-09-06 14:50:35 +0300

    begin form regisration

---
+++ server/index.js
@@ -0,0 +1,41 @@
+//Импортируем все зависимости дял работы сервера
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const authRouter = require('./routers/auth.routes')
+
+
+const app = express() //Создаем сервер
+
+// Получаем номер порта(Предварительно)
+// Создав папку с default.json 
+const PORT = config.get('serverPort')
+const URL = config.get('dbUrl')
+
+
+app.use(express.json())
+app.use('/api/auth', authRouter)
+
+
+// Функция для подключение к базе данных
+// И запуск сервера
+// try ... catch Для отловли ошибок
+// Делаем ее async т.к подключаемся к базе
+const start = async () => {
+    try {
+
+        await mongoose.connect(URL)
+
+        
+        // Пеер. параметр номер порта
+        // 2 параметр фукнция вызывается сразу посде
+        // запуска  сервера
+        app.listen(PORT, () => {
+            console.log('Server started on port', PORT);
+        })
+    } catch (error) {
+        
+    }
+}
+
+start()
---
+++ server/routers/auth.routes.js
@@ -0,0 +1,83 @@
+//Создаем маршруты, которые орабатываеют создание и авторитизацию пользователей
+// ВСЕ ФУНКЦИЯ АСИНХРОННЫ поэтому await
+
+const Router = require('express')
+const config = require('config')
+const User = require('../models/Users') // имортируем модель 
+const bcrypt = require('bcryptjs') // для hash пароля
+const {check, validationResult} = require("express-validator")
+const jwt = require('jsonwebtoken') // Создает токен
+const router = new Router() // обратка запроса
+
+// Обрабатываем пост запрос на регистрацию
+// req = Запрос, res = Ответ
+router.post('/registration', 
+    [
+        check('email', "Uncorrect email").isEmail(),
+        check('password', 'Password must be longer than 3 and shorter than 12').isLength({min:3, max:12})
+    ],
+    async (req, res) => {
+    try {
+        //Получаем результат валидации и проверяем есть ли ошбки
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({message: 'Uncorrect request', errors})
+        }
+
+        const {email, password} = req.body
+
+        const candidate = await User.findOne({email})
+
+        if(candidate) {
+            return res.status(400).json({message: `User with email: ${email} alredy exist!` })
+        }
+
+        // Защифровка пароля
+        const hashPassword = await bcrypt.hash(password, 7)
+        
+        const user =  new User({email, password: hashPassword})
+        await user.save()
+        return res.json({message: 'User was created!'})
+        
+    } catch (error) {
+        console.log(error)
+        res.send({message: 'Server error'})
+    }
+})
+
+router.post('/login', 
+    async (req, res) => {
+    try {
+        const secretKey = config.get('secretKey')
+
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(404).json({message: 'User not found'})
+        }
+
+        //Провека на совпадение Введенного и Зашифрованого пароля
+        const isPassValid = bcrypt.compareSync(password, user.password)
+        if(!isPassValid) {
+            return res.status(400).json({message: "Invalid password"})
+        }
+
+        const token = jwt.sign({id: user.id}, secretKey, {expiresIn: '1h'})
+        return res.json({
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                diskSpace: user.diskSpace,
+                usedSpace: user.usedSpace,
+                avatar: user.avatar
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.send({message: 'Server error'})
+    }
+})
+
+module.exports  = router

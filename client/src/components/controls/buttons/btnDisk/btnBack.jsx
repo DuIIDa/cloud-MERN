bb7a678cdeee78de6db9752bcaea11681347fb4e
Dudarev <dima_dudariev@mail.ru>
2020-09-29 12:47:02 +0300

    add create dir + ignore files

--- client/src/components/controls/buttons/btnDisk/btnBack.jsx
+++ client/src/components/controls/buttons/btnDisk/btnBack.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {setCurrentDir} from '../../../../actions/index'
 
 import arrowBack from '../../../../assets/img/btnBack.svg'
 import {Button} from './btnStyle'
 
 export const BtnBack = () => {
+    const dispatch = useDispatch()
+    const dirStack = useSelector(state=> state.files.dirStack)
+
+    const backHandler = () => {
+        const backDir = dirStack.pop() // Получем последний элемент
+        dispatch(setCurrentDir(backDir))
+    }
 
     return (
-        <Button>
-            <img src={arrowBack}/>
+        <Button onClick={() => backHandler()}>
+            <img src={arrowBack} alt = 'Back'/>
         </Button>
     )
 }
--- client/src/constants/action.js
+++ client/src/constants/action.js
@@ -5,3 +5,5 @@ export const SET_FILES = 'SET_FILES' // Для инициализации фай
 export const SET_СURRENT_DIR = 'SET_СURRENT_DIR' // Для текущей директории
 export const ADD_FILE = 'ADD_FILE' // Для создания файла
 export const SET_POPUP_DISPLAY = 'SET_POPUP_DISPLAY' // Для создания файла
+
+export const PUSH_TO_STACK = 'PUSH_TO_STACK' // Добавление в стек род. папок
